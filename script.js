c3b4eb4f5f9fba30ffa94ecf9ab53fad19027a66
Matheus Chaves <143457612+zzMatheusChaveszz@users.noreply.github.com>
2023-10-17 21:43:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let i = 1;
+while (i <= 50) {
+  console.log(i);
+  i++;
+}
+
+let contagem = 10;
+do {
+  console.log(i);
+  i--;
+} while (i >= 0);
+
+
+for (let i = 2; i <= 100; i += 2) {
+  console.log(i);
+}
